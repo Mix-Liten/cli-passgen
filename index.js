3b653df1f2072ffb0b0b2d19d61a993d040f09d7
Mix-Liten <mixliten@gmail.com>
2021-08-18 11:09:53 +0800

    feat: update cli

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const os = require("os");
 const program = require("commander");
 const chalk = require("chalk");
 const clipboardy = require("clipboardy");
@@ -5,39 +6,55 @@ const clipboardy = require("clipboardy");
 const createPassword = require("./utils/createPassword");
 const savePassword = require("./utils/savePassword");
 
-program.version("1.0.0").description("Simple Password Generator");
+program
+  .version("1.0.0")
+  .description("Simple Password Generator")
+  .usage("<command> [options]");
 
 program
+  .command("generate")
   .option("-l, --length <number>", "length of password", "8")
   .option("-s, --save", "save password to passwords.txt")
   .option("-nn, --no-numbers", "remove numbers")
   .option("-ns, --no-symbols", "remove symbols")
-  .parse();
+  .action((opts) => {
+    const { length, save, numbers, symbols } = opts;
 
-const { length, save, numbers, symbols } = program.opts();
+    if (length !== undefined && isNaN(length)) {
+      console.log(
+        chalk.red(
+          `error occurred in ${chalk.yellow(
+            "length"
+          )} option, it should be number type, but received: ${chalk.yellow(
+            length
+          )}`
+        )
+      );
+      process.exit();
+    }
 
-if (length !== undefined && isNaN(length)) {
-  console.log(
-    chalk.bgRed(
-      `error occurred in 'length' option, it should be number type, but received: ${length}`
-    )
-  );
+    // Get generated password
+    const generatedPassword = createPassword(length, numbers, symbols);
+
+    // Copy to clipboard
+    clipboardy.writeSync(generatedPassword);
+
+    // Save to file
+    if (save) {
+      savePassword(generatedPassword);
+    }
+
+    // Output generated password
+    console.log(
+      `${chalk.blue("Generated Password:")} ${chalk.bold(generatedPassword)}`
+    );
+    console.log(`${chalk.yellow("Password copied to clipboard")}`);
+  });
+
+program.on("command:*", ([cmd]) => {
+  program.outputHelp();
+  console.log(os.EOL + chalk.red(`Unknown command ${chalk.yellow(cmd)}.`));
   process.exit();
-}
+});
 
-// Get generated password
-const generatedPassword = createPassword(length, numbers, symbols);
-
-// Copy to clipboard
-clipboardy.writeSync(generatedPassword);
-
-// Save to file
-if (save) {
-  savePassword(generatedPassword);
-}
-
-// Output generated password
-console.log(
-  `${chalk.blue("Generated Password:")} ${chalk.bold(generatedPassword)}`
-);
-console.log(`${chalk.yellow("Password copied to clipboard")}`);
+program.parse(process.argv);
--- utils/savePassword.js
+++ utils/savePassword.js
@@ -9,7 +9,6 @@ const savePassword = (password) => {
    * fs.open 的第二個參數 flags，預設是 'r'，檔案不存在時會報錯，改為 'a'，檔案不存在時會建立檔案
    */
   fs.open(path.join(__dirname, "../", "passwords.txt"), "a", 666, (err, fd) => {
-    console.log(`fd`, fd);
     fs.write(fd, password + os.EOL, null, "utf-8", () => {
       fs.close(fd, () => {
         console.log(chalk.green("Password saved to passwords.txt"));
